1960d8a5584250d32bac5f31b928d813ae15bcec
Cheetack <ken.tsuchiya.7q@stu.hosei.ac.jp>
2020-07-23 20:52:27 +0900

    draw picture function.

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,25 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(810, 460);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainWindow_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainWindow_DragEnter);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseMove);
             // 
             // MainWindow
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
             this.Name = "MainWindow";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainWindow_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainWindow_DragEnter);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyUp);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- MainWindow.cs
+++ MainWindow.cs
@@ -20,6 +20,15 @@ namespace MultiAbilityCartoonExpressWindow
             InitializeComponent();
         }
 
+
+        private string getFileNameToDragEvent(DragEventArgs e)
+        {
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (System.IO.File.Exists(fileName[0]) == true) return fileName[0];
+            return null;
+        }
+
+
         /* 設定保存 */
 
         private void MainWindow_Load(object sender, EventArgs e)
@@ -34,7 +43,8 @@ namespace MultiAbilityCartoonExpressWindow
             Properties.Settings.Default.WindowWidthSetting = this.Height;
             Properties.Settings.Default.Save();
         }
-
+        
+        /* window操作関連 */
 
         private void MainWindow_MouseDown(object sender, MouseEventArgs e)
         {
@@ -68,5 +78,37 @@ namespace MultiAbilityCartoonExpressWindow
             if (e.KeyCode == Keys.Space) this.WindowState = FormWindowState.Minimized;
         }
 
+        /* D&D関連 */
+
+        private void MainWindow_DragDrop(object sender, DragEventArgs e)
+        {
+            string fileName = this.getFileNameToDragEvent(e);
+            Console.WriteLine(fileName);
+
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            //画像ファイルを読み込んで、Imageオブジェクトとして取得する
+            Image img = Image.FromFile(fileName);
+
+            g.DrawImage(img, 0, 0, img.Width, img.Height);
+            //Imageオブジェクトのリソースを解放する
+            img.Dispose();
+
+            //Graphicsオブジェクトのリソースを解放する
+            g.Dispose();
+            //pictureBox1に表示する
+            pictureBox1.Image = canvas;
+
+        }
+
+        private void MainWindow_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.All;
+            else e.Effect = DragDropEffects.None;
+
+        }
     }
 }
